f580c3062abafa62035d172aaa313e1a8c2b1059
Priyanshu <priyanshuwks@gmail.com>
2023-08-15 13:00:52 +0530

    Now, plus & minus buttons appears perfectly in the single product

--- src/components/singleProduct/SingleProduct.js
+++ src/components/singleProduct/SingleProduct.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {BsCurrencyRupee} from 'react-icons/bs';
 import './SingleProduct.css'
-// import {AiOutlineMinus} from 'react-icons/ai'
-// import {AiOutlinePlus} from 'react-icons/ai'
-// import { useDispatch, useSelector } from "react-redux";
-// import { addToCart, removeFromCart } from "../../redux/slices/CartSlice";
+import {AiOutlineMinus} from 'react-icons/ai'
+import {AiOutlinePlus} from 'react-icons/ai'
+import { useDispatch, useSelector } from "react-redux";
+// import { removeFromCart } from "../../redux/slices/CartSlice";
 
 function SingleProduct({ product }) {
   // const dispatch = useDispatch();
@@ -17,11 +17,11 @@ function SingleProduct({ product }) {
       <img src={product.image} alt="" className="imageStyle"/>
       <h3 className="titleStyle">{product.title}</h3>
       <h4 className="priceStyle"><BsCurrencyRupee/>{ product.price * 80}</h4>
-      {/* <div className="plusMinusDiv">
-        <button className="minusButton buttons" onClick={dispatch(removeFromCart(product.id))}><AiOutlineMinus/></button>
-        <h4 id="noOfItems">{currQuantity}</h4>
-        <button className="plusButton buttons" onClick={dispatch(addToCart(product.id))}><AiOutlinePlus/></button>
-      </div> */}
+      <div className="plusMinusDiv">
+        <button className="minusButton buttons" ><AiOutlineMinus/></button>
+        <h4 id="noOfItems">0</h4>
+        <button className="plusButton buttons" ><AiOutlinePlus/></button>
+      </div>
     </div>
   );
 }
