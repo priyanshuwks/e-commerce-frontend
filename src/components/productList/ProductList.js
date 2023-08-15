f58e88ed18dfab56c45c2abf2df21675649fa166
Priyanshu <priyanshuwks@gmail.com>
2023-08-16 01:11:58 +0530

    used redux thunk instead of sync redux

--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -1,21 +1,27 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadProducts } from "../../redux/slices/ProductSlice";
+import { fetchData} from "../../redux/slices/ProductSlice";
 import SingleProduct from "../singleProduct/SingleProduct";
 import './ProductList.css';
+import { Spin } from 'antd';
 
 function ProductList() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.productReducer.products);
+  const status = useSelector(state => state.productReducer.status);
+  
   useEffect(() => {
-    fetchData();
+    dispatch(fetchData())
   }, []);
 
-  async function fetchData() {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
-    dispatch(loadProducts(data));
+  if(status === 'loading'){
+    return <Spin className='loading'/>
   }
+
+  if(status === 'failed'){
+    return <h2 className="loading">Oh! no Something Went Wrong...</h2>
+  }
+
   return (
     <div className="singleProduct">
       {products.map((item) => {
